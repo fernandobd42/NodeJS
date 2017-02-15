9c41bcf279cae288f569d8a3ad8377b4ccb6bf0c
fernandobd43 <fernandolucasbd@gmail.com>
2017-02-15 16:17:59 -0500

    iniciando com ejs e express

---
+++ app.js
@@ -0,0 +1,19 @@
+var app = require('./config/server');
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.render("home/index")
+});
+
+app.get('/formulario', function(req, res){
+  res.render("admin/form_add_noticia")
+});
+
+app.get('/noticias', function(req, res){
+  res.render("noticias/noticias")
+});
+
+app.listen(3000, function(){
+  console.log('Servidor Online')
+});
