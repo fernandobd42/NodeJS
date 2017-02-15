e7e4cca67c15a6536838422e038b9c4cbf68b4ef
fernandobd43 <fernandolucasbd@gmail.com>
2017-02-15 16:20:29 -0500

    moduralizando/abstraindo codigo

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 var app = require('./config/server');
 
-app.set('view engine', 'ejs');
+var rotaHome = require('./app/routes/home')(app);
 
-app.get('/', function(req, res){
-  res.render("home/index")
-});
+var rotaNoticias = require('./app/routes/noticias')(app);
 
-app.get('/formulario', function(req, res){
-  res.render("admin/form_add_noticia")
-});
-
-app.get('/noticias', function(req, res){
-  res.render("noticias/noticias")
-});
+var rotaFormulario = require('./app/routes/formulario')(app);
 
 app.listen(3000, function(){
   console.log('Servidor Online')
